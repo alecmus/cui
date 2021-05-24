alecmus/cui

commit f900d3ada037ed62c3e723756adee084653ba483

Tooltips now DPI aware.

cui_raw/cui_raw.cpp
@@ -84,7 +84,7 @@ cui_raw::cui_raw(
 
 	// capture tooltip parameters
 	d->m_sTooltipFont = sTooltipFont;
-	d->m_iTooltipFontSize = iTooltipFontSize;
+	d->m_iTooltipFontSize = iTooltipFontSize * d->m_DPIScale;
 	d->m_clrTooltipText = clrTooltipText;
 	d->m_clrTooltipBackground = clrTooltipBackground;
 	d->m_clrTooltipBorder = clrTooltipBorder;

cui_raw/cui_rawImpl/cui_rawImpl.cpp
@@ -100,6 +100,7 @@ cui_rawImpl::cui_rawImpl()
 	m_iMinWidthCalc = 30;
 
 	// scale for DPI
+	m_iTooltipFontSize = m_iTooltipFontSize * m_DPIScale;
 	m_iTitlebarHeight = int(0.5 + m_iTitlebarHeight * m_DPIScale);
 	m_iMinWidthCalc = int(0.5 + m_iMinWidthCalc * m_DPIScale);
 

cui_raw/cui_rawImpl/Image/Image.cpp
@@ -507,7 +507,7 @@ LRESULT CALLBACK cui_rawImpl::ImageProc(HWND hWnd, UINT msg, WPARAM wParam, LPAR
 				}
 
 				Gdiplus::Graphics graphics_out(hdc);
-				graphics_out.DrawImage(original, pControl->rcImage.left, pControl->rcImage.top);
+				graphics_out.DrawImage(original, (Gdiplus::REAL)pControl->rcImage.left, (Gdiplus::REAL)pControl->rcImage.top);
 
 				if (original)
 				{
@@ -1040,7 +1040,7 @@ LRESULT CALLBACK cui_rawImpl::ImageProc(HWND hWnd, UINT msg, WPARAM wParam, LPAR
 				}
 
 				Gdiplus::Graphics graphics_out(hdc);
-				graphics_out.DrawImage(original, pControl->rcImage.left, pControl->rcImage.top);
+				graphics_out.DrawImage(original, (Gdiplus::REAL)pControl->rcImage.left, (Gdiplus::REAL)pControl->rcImage.top);
 
 				if (original)
 				{

versioninfo.h
@@ -24,4 +24,4 @@
 #define cuiminor		0
 #define cuirevision		1
 
-#define cuidate			"16 Nov 2020"
+#define cuidate			"24 May 2021"

commit d5065b00426871793870f0ba85922b8a51b39a9f

Fixed bug introduced in f900d3ada037ed62c3e723756adee084653ba483 that caused ever increasing tooltip font size if DPI awareness is set using the manifest.

cui_raw/cui_raw.cpp
@@ -62,7 +62,7 @@ cui_raw::cui_raw(
 	void* pState
 )
 {
-	d = new cui_rawImpl;
+	d = new cui_rawImpl(sPageName);
 
 	d->m_sCurrentPage = sPageName;
 
@@ -84,7 +84,7 @@ cui_raw::cui_raw(
 
 	// capture tooltip parameters
 	d->m_sTooltipFont = sTooltipFont;
-	d->m_iTooltipFontSize = iTooltipFontSize * d->m_DPIScale;
+	d->m_iTooltipFontSize = d->m_dpi_awareness_set_programmatically ? iTooltipFontSize * d->m_DPIScale : iTooltipFontSize;
 	d->m_clrTooltipText = clrTooltipText;
 	d->m_clrTooltipBackground = clrTooltipBackground;
 	d->m_clrTooltipBorder = clrTooltipBorder;

cui_raw/cui_rawImpl/cui_rawImpl.cpp
@@ -23,15 +23,17 @@
 #define to_tstring	std::to_string
 #endif // _UNICODE
 
-cui_rawImpl::cui_rawImpl()
+cui_rawImpl::cui_rawImpl(const std::wstring& title)
 {
-	if (!IsProcessDPIAware())
-	{
+	if (title != L"cui default window" && !IsProcessDPIAware()) {
+		m_dpi_awareness_set_programmatically = true;
 		if (!SetProcessDPIAware())
 			MessageBox(nullptr,
 				L"This program is not DPI aware. As a result, UI elements may not be clear.",
 				L"liblec::cui::gui_raw::cui_raw", MB_ICONWARNING);
 	}
+	else
+		m_dpi_awareness_set_programmatically = false;
 
 	m_hWnd = NULL;
 	m_hWndParent = NULL;
@@ -100,7 +102,7 @@ cui_rawImpl::cui_rawImpl()
 	m_iMinWidthCalc = 30;
 
 	// scale for DPI
-	m_iTooltipFontSize = m_iTooltipFontSize * m_DPIScale;
+	m_iTooltipFontSize = m_dpi_awareness_set_programmatically ? m_iTooltipFontSize * m_DPIScale : m_iTooltipFontSize;
 	m_iTitlebarHeight = int(0.5 + m_iTitlebarHeight * m_DPIScale);
 	m_iMinWidthCalc = int(0.5 + m_iMinWidthCalc * m_DPIScale);
 

cui_raw/cui_rawImpl/cui_rawImpl.h
@@ -36,7 +36,8 @@ using namespace liblec::cui::gui_raw;
 class liblec::cui::gui_raw::cui_rawImpl
 {
 public:
-	cui_rawImpl();
+	cui_rawImpl() = delete;
+	cui_rawImpl(const std::wstring& title);
 	~cui_rawImpl();
 
 	///////////////////////////////////////////////////////////////////////////////////////
@@ -993,6 +994,7 @@ public:
 	// tooltip font parameters
 	std::basic_string<TCHAR> m_sTooltipFont;
 	double m_iTooltipFontSize;
+	bool m_dpi_awareness_set_programmatically;
 	COLORREF m_clrTooltipText;
 	COLORREF m_clrTooltipBackground;
 	COLORREF m_clrTooltipBorder;